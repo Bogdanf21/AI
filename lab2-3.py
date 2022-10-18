645c3ed24c2e2c695abc25d1724527193b14fde9
bogdanf21 <bogdanfomin21@gmail.com>
2022-10-18 18:17:39 +0300

    heuristics needed

--- lab2-3.py
+++ lab2-3.py
@@ -1,3 +1,4 @@
+import sys
 from collections import deque
 from email.policy import default
 
@@ -112,11 +113,21 @@ class Lab2Problem:
                 break
 
             for possible_next_transition in self.possibleMoves(transition):
-                if self.validate(transition, possible_next_transition): #adaug doar tranzitiile valide
+                if self.validate(transition, possible_next_transition):  # adaug doar tranzitiile valide
+                    if self.isFinal(possible_next_transition):
+                        parent[(possible_next_transition[0], possible_next_transition[1])] = (
+                            transition[0], transition[1])
+                        is_solvable = True
+                        solution = self.__trace_back(parent, (possible_next_transition[0], possible_next_transition[1]))
+                        for i in solution:
+                            print(i)
+                        break
                     queue.append(possible_next_transition)
                     if (possible_next_transition[0], possible_next_transition[1]) not in parent.keys():
                         parent[(possible_next_transition[0], possible_next_transition[1])] = (
                             transition[0], transition[1])
+            if is_solvable:
+                break
 
         if not is_solvable:
             print("No solution found")
@@ -127,7 +138,7 @@ class Lab2Problem:
         queue = deque([self.initialize(self.m, self.n, self.k)])
         parent = {(0, 0): (0, 0)}
         visited = {}
-        
+
         previous_transition = transition = queue[0]
 
         while len(queue) > 0:
@@ -135,7 +146,6 @@ class Lab2Problem:
             if (transition[0], transition[1]) in visited:
                 continue
 
-            
             visited[(transition[0], transition[1])] = 1
 
             if self.isFinal(transition):
@@ -143,20 +153,31 @@ class Lab2Problem:
                 solution = self.__trace_back(parent, (transition[0], transition[1]))
                 for i in solution:
                     print(i)
-                break
 
             for possible_next_transition in self.possibleMoves(transition):
                 if self.validate(transition, possible_next_transition):
-                    queue.appendleft(possible_next_transition)
-                    if (possible_next_transition[0], possible_next_transition[1]) not in parent.keys():
+                    if self.isFinal(possible_next_transition):
                         parent[(possible_next_transition[0], possible_next_transition[1])] = (
                             transition[0], transition[1])
+                        is_solvable = True
+                        solution = self.__trace_back(parent, (possible_next_transition[0], possible_next_transition[1]))
+                        for i in solution:
+                            print(i)
+                    else:
+                        queue.appendleft(possible_next_transition)
+                        if (possible_next_transition[0], possible_next_transition[1]) not in parent.keys():
+                            parent[(possible_next_transition[0], possible_next_transition[1])] = (
+                                transition[0], transition[1])
 
         if not is_solvable:
             print("No solution found")
 
+    def __hill_climb_heuristic(self, state):
+        return abs(state[0] - state[4]) + abs(state[1] - state[4])
 
-    def __heuristic(self, state):
+    def __a_star_heuristic(self, state):
+        if abs(state[0] - state[4]) + abs(state[1] - state[4]) == 0:
+            return sys.maxsize
         return abs(state[0] - state[4]) + abs(state[1] - state[4])
 
     def hillclimb(self):
@@ -164,21 +185,16 @@ class Lab2Problem:
         steps = 0
         print("##### HillClimb SOLUTION #####")
         initial_state = self.initialize(self.m, self.n, self.k)
-        queue = [initial_state]
+        transition = initial_state
         parent = {(initial_state[0], initial_state[1]): (initial_state[0], initial_state[1])}
-        parent_heuristic_value = self.__heuristic(initial_state)
         visited = {}
-        
 
-        while len(queue) > 0:
-            steps += 1
-            transition = queue.pop(0)
-            parent_heuristic_value = self.__heuristic(transition)
+        while True:
+            parent_heuristic_value = self.__hill_climb_heuristic(transition)
 
             if (transition[0], transition[1]) in visited:
                 continue
 
-            
             visited[(transition[0], transition[1])] = 1
 
             if self.isFinal(transition):
@@ -186,42 +202,44 @@ class Lab2Problem:
                 solution = self.__trace_back(parent, (transition[0], transition[1]))
                 for i in solution:
                     print(i)
-                print("Steps: ", steps)
                 break
 
             transitions = self.possibleMoves(transition)
-            transitions = list(t for t in transitions if self.__heuristic(t) <= parent_heuristic_value)
-            transitions.sort(key=self.__heuristic, reverse=True)
+            transitions = list(t for t in transitions if self.__hill_climb_heuristic(t) <= parent_heuristic_value)
+            transitions.sort(key=self.__hill_climb_heuristic)
+            heuristics = list(self.__hill_climb_heuristic(t) for t in transitions)
 
-            for possible_next_transition in transitions:
-                if self.validate(transition, possible_next_transition):
-                    queue.append(possible_next_transition)
-                    if (possible_next_transition[0], possible_next_transition[1]) not in parent.keys():
-                        parent[(possible_next_transition[0], possible_next_transition[1])] = (
-                            transition[0], transition[1])
+            possible_next_transition = None
+
+            for t in transitions:
+                if (t[0], t[1]) not in visited:
+                    possible_next_transition = t
+                    break
+
+            if possible_next_transition is None:
+                break
+
+            parent[(possible_next_transition[0], possible_next_transition[1])] = (
+                transition[0], transition[1])
+            parent_heuristic_value = self.__hill_climb_heuristic(transition)
+            transition = possible_next_transition
 
         if not is_solvable:
             print("No solution found")
 
     def a_star(self):
-        steps = 0
         is_solvable = False
         print("##### A* SOLUTION #####")
-        initial_state = self.initialize(self.m, self.n, self.k)
-        queue = [initial_state]
-        parent = {(initial_state[0], initial_state[1]): (initial_state[0], initial_state[1])}
+        queue = [self.initialize(self.m, self.n, self.k)]
+        parent = {(0, 0): (0, 0)}
         visited = {}
-        
+        previous_transition = transition = queue[0]
 
         while len(queue) > 0:
-            steps += 1
             transition = queue.pop(0)
-            parent_heuristic_value = self.__heuristic(transition)
-
             if (transition[0], transition[1]) in visited:
                 continue
 
-        
             visited[(transition[0], transition[1])] = 1
 
             if self.isFinal(transition):
@@ -229,163 +247,67 @@ class Lab2Problem:
                 solution = self.__trace_back(parent, (transition[0], transition[1]))
                 for i in solution:
                     print(i)
-                break
 
             for possible_next_transition in self.possibleMoves(transition):
-                if self.validate(transition, possible_next_transition):
+                if self.validate(transition, possible_next_transition):  # adaug doar tranzitiile valide
+                    if self.isFinal(possible_next_transition):
+                        parent[(possible_next_transition[0], possible_next_transition[1])] = (
+                            transition[0], transition[1])
+                        is_solvable = True
+                        solution = self.__trace_back(parent, (possible_next_transition[0], possible_next_transition[1]))
+                        print("--Solution--")
+                        for i in solution:
+                            print(i)
+                        print("\n")
+                        break
+
                     queue.append(possible_next_transition)
                     if (possible_next_transition[0], possible_next_transition[1]) not in parent.keys():
                         parent[(possible_next_transition[0], possible_next_transition[1])] = (
                             transition[0], transition[1])
 
-            queue.sort(key=self.__heuristic, reverse=True)
+            queue.sort(key=self.__hill_climb_heuristic, reverse=True)
 
         if not is_solvable:
             print("No solution found")
 
 
 def menu():
-        
     continuee = True
+    m = int(input("Enter m:"))
+    n = int(input("Enter n:"))
+    k = int(input("Enter k:"))
 
-    while continuee :
-        m= int(input("Enter m:" ))
-        n= int(input("Enter n:" ))
-        k= int(input("Enter k:" ))
-        
+    while continuee:
+
 
         print("Choose the strategy you want")
-        
+
         print("1.bkt")
         print("2.bfs")
         print("3.hillclimbing")
-        print("4.A*") 
-    
-        option= int(input("number:" ))
-        
-        solver= Lab2Problem(m,n,k)
+        print("4.A*")
+        option = int(input("number:"))
+
+        solver = Lab2Problem(m, n, k)
 
         match option:
-            case 1: solver.bktt()
-            case 2: solver.bfs()
-            case 3: solver.hillclimb()
-            case 4: solver.a_star() 
-            
-            case _: print("enter a valid option")
-        
-        var= input("Want to try again?(y/n)")
+            case 1:
+                solver.bktt()
+            case 2:
+                solver.bfs()
+            case 3:
+                solver.hillclimb()
+            case 4:
+                solver.a_star()
 
-        if var == 'n' :
-            continuee= False
-    
+            case _:
+                print("enter a valid option")
+
+        var = input("Want to try again?(y/n)")
+
+        if var == 'n':
+            continuee = False
+
 
 menu()
-
-# solver = Lab2Problem(4, 5, 3)
-# solver.bktt()
-# solver.bfs()
-# solver.hillclimb()
-# solver.a_star()
-
-# def bkt(self):
-#     print("##### BACKTRACKING SOLUTION #####")
-#     state = self.initialize(self.m, self.n, self.k)
-#     self.__runbkt(state, {}, {})
-#
-#
-# def __runbkt(self, state, visited, parent_map):
-#     if self.isFinal(state):
-#         solution = self.__trace_back(parent_map, (state[0], state[1]))
-#         for i in solution:
-#             print(i)
-#         print("\n")
-#         return True
-#     else:
-#         for possible_next_transition in self.possibleMoves(state):
-#             if self.validate(state, possible_next_transition) and \
-#                     (possible_next_transition[0], possible_next_transition[1]) not in visited:
-#                 visited[(possible_next_transition[0], possible_next_transition[1])] = 1
-#                 parent_map[(possible_next_transition[0], possible_next_transition[1])] = (state[0], state[1])
-#                 return self.__runbkt(possible_next_transition, visited, parent_map)
-
-# statee = [8, 5, 8, 4, 2]
-# possible = solver.possibleMoves(statee)
-# print("possible moves:", possible)
-# print("valid moves:")
-# for move in possible:
-#     if (solver.validate(statee, move)):
-#         print(move)
-
-# print("is final:", solver.isFinal(statee))
-# print("is emptying glass 1 ok", solver.validate(statee, 0, 4))
-# print("is emptying glass 2 ok", solver.validate(statee, 0, 0))
-# print("glass1 <- glass2: ", solver.validate(statee, 3, 2))
-# print("glass1 -> glass2: ", solver.validate(statee, 1, 5))
-# # state = solver.transition(state, 1, 0)
-# print("glass1 -> glass2: ", statee)
-
-# def validate(self, state, reciever_position, giver_position):
-#     #receiver can have the values -1, 0, 1, giver 0, 1
-#     if reciever_position > 1 or giver_position > 1 or giver_position == reciever_position:
-#         print("Receiver position or giver position are invalid")
-#         return False
-#     if state[giver_position] == 0:  # giver doesn't have water
-#         return False
-#     if reciever_position < 0:  # giver is spilled on the floor
-#         return True
-#
-#     receiver_current_quantity = state[reciever_position]
-#     receiver_max_quantity = state[reciever_position + 2]
-#
-#     if receiver_max_quantity != receiver_current_quantity:
-#         return True
-
-# def transition(self, state, reciever_position, giver_position):
-#     if reciever_position < 0:
-#         state[giver_position] = 0
-#     else:
-#         receiver_current_quantity = state[reciever_position]
-#         receiver_max_quantity = state[reciever_position + 2]
-#
-#         giver_current_quantity = state[giver_position]
-#         giver_max_quantity = state[giver_position + 2]
-#
-#         transferred_quantity = min(receiver_max_quantity - receiver_current_quantity, giver_current_quantity)
-#         state[reciever_position] += transferred_quantity
-#         state[giver_position] -= transferred_quantity
-#     return state
-
-
-# def validate2(self, state, cup1_new_value, cup2_new_value):
-#     if cup1_new_value < 0 or cup2_new_value < 0:
-#         return False
-#     cup1_current_value = state[0]
-#     cup2_current_value = state[1]
-#
-#     if cup1_new_value == 0 and cup1_current_value != 0 and cup2_new_value == state[1]:  # cup1 was spilled and
-#         # cup2 stayed the same
-#         return True
-#
-#     if cup1_new_value == cup1_current_value and cup2_new_value == 0 and cup2_current_value != 0:
-#         #  cup1 stayed the same
-#         #  and cup2 was spilled
-#         return True
-#
-#     cup1_max_value = state[2]
-#
-#     cup2_max_value = state[3]
-#
-#     # c1 to c2:
-#     transferred_value1 = min(cup2_max_value - cup2_current_value, cup1_current_value)
-#     if cup2_new_value == cup2_current_value + transferred_value1 and \
-#             cup1_new_value == cup1_current_value - transferred_value1 and \
-#             transferred_value1 > 0:  # check if the move is redundant or not
-#         return True
-#
-#     # c2 to c1
-#     transferred_value2 = min(cup1_max_value - cup1_current_value, cup2_current_value)
-#     if cup1_new_value == cup1_current_value + transferred_value2 and \
-#             cup2_new_value == cup2_current_value - transferred_value2 and \
-#             transferred_value2 > 0:
-#         return True
-#     return False
