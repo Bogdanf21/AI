cafe84cfa8bcc731f9545e54ca498467d86b0262
bogdanf21 <bogdanfomin21@gmail.com>
2022-11-25 10:40:19 +0200

    Lab9 until C) + bonus plotting

--- lab9.py
+++ lab9.py
@@ -1,16 +1,10 @@
 import random
+import time
 from enum import Enum
 import gymnasium as gym
 import numpy as np
+import matplotlib.pyplot as plt
 
-#
-#
-# class StateType(Enum):
-#     START = 1
-#     NOTHING = 2
-#     CLIFF = 3
-#     END = 4
-#
 LINES = 4
 COLUMNS = 12
 ACTIONS = 4
@@ -35,11 +29,6 @@ env = gym.make('CliffWalking-v0')
 env.reset()
 
 
-
-# def get_initial_state(a_state):
-#     print(a_state[0])
-#     i = a_state / 12
-
 def get_next_action(a_state, an_action):
     if a_state not in range(0, 38):
         raise ValueError(f"STATE NOT IN RANGE 0 38 {a_state}")
@@ -62,36 +51,36 @@ state = env.reset()[0]  # value of the initial state
 steps = 1
 done_perfectly = 0
 
+start_time = time.process_time()
+# for stats
+x = []
+y = []
+# ####
 for i in range(EPOCHS):
     done = False
-    seed = random.randrange(10**5, 10**6)
     state = env.reset()[0]
-    print(f"EPOCH: {i}")
+    # print(f"EPOCH: {i}")
+    # for stats
     steps = 0
+    rewards = []
+    # ##########
     while not done:
-        # print(f"Mod value: {int(10000*1/steps)}")
-        # will_be_random = 0 if seed % int(10*1/steps + 1) == 0 else 1
-        # if will_be_random:
-        #     action = random.randrange(0, 4)
-        # else:
         action = np.argmax(q_table[state])
 
         new_state, reward, done, _, _ = env.step(action)
-        print(f"NEW_STATE = {new_state} OLD_STATE = {state}")
-        # if new_state == state:
-        #     q_table[state][action] = -200
-        #     print(f"Action {action} state {new_state}")
+       # print(f"NEW_STATE = {new_state} OLD_STATE = {state}")
 
         env.render()
+        rewards.append(reward)
 
         if not done:
-            print(f"POSSIBLE Q'S: {q_table[new_state]}")
+            # print(f"POSSIBLE Q'S: {q_table[new_state]}")
             max_future_q = np.max(q_table[new_state])
             # g_n_a = get_next_action(state, action) only in env we stepped on, so we can do this
             g_n_a = new_state
             current_q = q_table[g_n_a][action]
             new_q = (1 - LEARNING_RATE) * current_q + LEARNING_RATE * (reward + DISCOUNT * max_future_q)
-            print(f"NEW_Q {new_q}")
+            #  print(f"NEW_Q {new_q}")
             q_table[state][action] = new_q
             # if it goes off
             if new_state in range(36, 47):
@@ -106,17 +95,17 @@ for i in range(EPOCHS):
 
         steps += 1
         state = new_state
-    if i == EPOCHS-1:
+    # for stats
+    time_elapsed = time.process_time()
+    avg_reward = sum(rewards)/len(rewards)
+    print(f"Avg_reward for this epoch: {avg_reward}")
+    x.append(time_elapsed)
+    y.append(avg_reward)
+
+    if i == EPOCHS - 1:
         print("DONE")
+    ######
 
-print("ACCURACY: ", done_perfectly/EPOCHS)
-
-
-
-
-#env.close()
-#
-# def initial_state():
-#         # of type [x, y, StateType, CurrentScore]
-#     pass
-# print(q_table)
+plt.plot(x, y)
+plt.show()
+print("ACCURACY: ", done_perfectly / EPOCHS)
